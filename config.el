d205284379f3f4cac2c79e0eca76ee5727d15253
sschob <stoeff00@gmail.com>
2020-04-21 16:00:09 +0200

    load package org-download and add beamerswitch

--- config.el
+++ config.el
@@ -97,4 +97,29 @@
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
- )
+  (add-to-list 'org-latex-classes
+               '("beamerS" "\\documentclass[presentation]{beamerswitch}"
+                ("\\section{%s}" . "\\section*{%s}")
+                ("\\subsection{%s}" . "\\subsection*{%s}")
+                ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+               ))
+
+(use-package! org-download
+  :commands
+  org-download-dnd
+  org-download-yank
+  org-download-screenshot
+  org-download-dnd-base64
+  :config
+  (setq org-download-image-dir "./images"
+        org-download-link-format "[[file:%s]]\n"
+        org-download-method 'directory
+        org-download-heading-lvl nil
+        org-download-timestamp "_%Y%m%d_%H%M%S"
+        org-download-screenshot-method
+        (cond (IS-MAC "screencapture -i %s")
+              (IS-LINUX
+               (cond ((executable-find "maim")  "maim -s %s")
+                     ((executable-find "scrot") "scrot -s %s")
+                     ((executable-find "gnome-screenshot") "gnome-screenshot -a -f %s")))))
+  )
