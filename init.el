7914f28471bb87c128b79ded17b0ba11818067a8
Stoeff <stoeff00@gmail.com>
2024-09-02 09:31:21 +0200

    enable roam2

--- init.el
+++ init.el
@@ -153,7 +153,9 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +jupyter)      ; organize your plain life in plain text
+       (org                ;organize your plain life in plain text
+        +jupyter           ; jupyter integration
+        +roam2)            ; Zettelkasten
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
